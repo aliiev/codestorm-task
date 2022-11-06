1cf3821c64e2a5991829f514b016010a0dbd266e
aliiev <aliiev.akhtem@gmail.com>
2022-11-06 23:25:21 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <h2>Hello world</h2>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis error molestias accusantium quas. Necessitatibus hic est, qui temporibus quasi commodi praesentium, quod veniam laboriosam voluptas tenetur impedit. Dolorum, quod quibusdam.</p>
     </div>
-  );
+  )
 }
-
-export default App;
